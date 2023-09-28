0439e1fb1a9d4f28f94bbd4afceca5f16fc6587c
Omar Gutiérrez <133981938+OmarGtze@users.noreply.github.com>
2023-09-27 23:18:00 -0600

    Add files via upload

---
+++ arduino1.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>  
+
+int valor = 0;  
+Servo serv; 
+void setup() { 
+  serv.attach(9); 
+} 
+void loop() {
+  valor = analogRead(0);   
+  if(690>valor){    
+   serv.write(60);        
+  }
+  else{  
+   serv.write(90);
+  } 
+  delay(5);
+}
